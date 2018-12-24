541de14610eba399784929ba8b8e2b044f12e78b
zm <13167008176@163.com>
2018-12-24 17:11:54 +0800

    [task] 新增home、mall、mine、msg module

---
+++ module_home/src/main/java/com/zm/home/debug/HomeDebugActivity.java
@@ -0,0 +1,15 @@
+package com.zm.home.debug;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zm.common.R;
+
+public class HomeDebugActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debug);
+    }
+}
---
+++ module_msg/src/main/java/com/zm/msg/debug/DebugMsgActivity.java
@@ -0,0 +1,15 @@
+package com.zm.msg.debug;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.zm.msg.R;
+
+public class DebugMsgActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_debug_msg);
+    }
+}
